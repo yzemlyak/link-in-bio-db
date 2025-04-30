c3557b5315003681accdfde93f9f2f17ac2d0972
yzemlyak <yzemlyak@gmail.com>
2025-04-30 02:01:19 +0000

    Version for espn done

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,5 +1,6 @@
 class ZebraController < ApplicationController
   def giraffe
+    @list_of_items = Item.all.order({:created_at => :desc})
     render({:template => "item_templates/list"})
   end 
 end
